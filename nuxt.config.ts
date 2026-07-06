6a6f2156c90a9fef73274cabd32036c1a904e6b5
AchahbarIlias <ilias.achahbar@outlook.com>
2026-07-06 12:59:59 +0200

    fix: remove unused stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
 
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -10,7 +9,9 @@ export default defineNuxtConfig({
     '~/assets/main.css'
   ],
 
-  modules: ['@nuxt/content'],
+  modules: [
+    '@nuxt/content',
+  ],
 
   vite: {
     optimizeDeps: {
